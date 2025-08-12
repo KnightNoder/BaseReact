e7a63ae19f9c2a1024b48fe093cace1ebf679e0d
EnsLapcon2 <rnalika@drcloudehr.com>
2025-08-12 13:59:50 +0530

    can add tailwind custom fileds, axios and react-query installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-green-500 h-screen w-full flex justify-center items-center">
-        Hello Lava
+      <div className="text-primary border-2 border-amber-400 rounded-10xl text-5xl h-screen w-full flex justify-center items-center">
+        <button className="border-2 px-20 rounded-10xl py-6 border-red-300 ">
+          Hello lava
+        </button>
       </div>
     </>
   );
